b0b3d255f3f0540263b78a51bfe2a327825671c4
ngrav007 <ngravel007@gmail.com>
2023-12-22 16:45:11 +0000

    docs: remove comments in code

--- src/main.rs
+++ src/main.rs
@@ -18,30 +18,15 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        // shadow the previous guess variable and convert the String type to integer
+        // shadow / convert the previous value of 'guess' to a u32 type
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue, // the '_' is a catchall, it matches with *all* Err values
         };
 
         println!("You guessed: {guess}");
-        /*
-         * The {} set of curly brackets is a placeholder: think of {} as little crab
-         * pincers that hold a value in place. When printing the value of a
-         * variable, the variable name can go inside the curly brackets. When
-         * printing the result of evaluating an expression, place empty curly
-         * brackets in the format string, then follow the format string with a
-         * comma-separated list of expressions to print in each empty curly bracket
-         * placeholder in the same order. Printing a variable and the result of an
-         * expression in one call to println! would look like this:
-         *
-         *      let x = 5;
-         *      let y = 10;
-         *      println!("x = {x} and y + 2 = {}", y + 2);
-         *
-         * This code would print x = 5 and y + 2 = 12.
-         */
 
+        // check guess
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
